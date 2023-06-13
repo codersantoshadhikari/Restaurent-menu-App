ce6b82a42ffb85ac4703c0bc7a64cfeb4293e886
santoshadhikary <santoshadhikari.name.np@gmail.com>
2023-06-13 11:47:12 +0545

    final

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      color: Colors.black12,
       debugShowCheckedModeBanner: false,
       title: "Resturent App",
       home: HomePage(),
